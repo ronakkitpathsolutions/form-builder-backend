20d44d793cc30329d65060f6ff26775116e3a1f4
Ronak Kapadi <ronakk@itpathsolutions.com>
2023-06-28 18:16:38 +0530

    Create get all access APIs.

--- src/controllers/user-management/access/index.js
+++ src/controllers/user-management/access/index.js
@@ -13,6 +13,57 @@ class AccessController {
 		dotenv.config()
 	}
 
+	getAllAccess = async (req, res) => {
+		try {
+			const data = await Access.find({}).select(['_id', 'role_type', 'access'])
+			if (!data) {
+				return res.status(statusCode.notFound).json(
+					response({
+						type: types.error,
+						message: 'Not found.'
+					})
+				)
+			}
+			return res.status(statusCode.success).json(
+				response({
+					type: types.success,
+					data
+				})
+			)
+		} catch (error) {
+			serverError(error, res)
+		}
+	}
+
+	getAccess = async (req, res) => {
+		try {
+			const { _id } = req.params
+			const data = await Access.findById(_id).select([
+				'_id',
+				'role_type',
+				'access'
+			])
+
+			if (!data) {
+				return res.status(statusCode.notFound).json(
+					response({
+						type: types.error,
+						message: 'Not found.'
+					})
+				)
+			}
+
+			return res.status(statusCode.success).json(
+				response({
+					type: types.success,
+					data
+				})
+			)
+		} catch (error) {
+			serverError(error, res)
+		}
+	}
+
 	createAccess = async (req, res) => {
 		try {
 			const { role_type, access } = req.body
@@ -51,6 +102,47 @@ class AccessController {
 				response({
 					type: types.success,
 					data: accessData
+				})
+			)
+		} catch (error) {
+			serverError(error, res)
+		}
+	}
+
+	updateAccess = async (req, res) => {
+		try {
+			const { _id } = req.params
+			const isAllFieldRequired = Helper.allFieldsAreRequired(
+				Object.values(req.body)
+			)
+
+			if (isAllFieldRequired)
+				return res.status(statusCode.badRequest).json(
+					response({
+						type: types.error,
+						message: 'All fields are required.'
+					})
+				)
+
+			await Access.findByIdAndUpdate(
+				_id,
+				Helper.removeField(['_id', 'role_type'], { ...req.body })
+			)
+			const data = await Access.findById(_id)
+
+			if (!data)
+				return res.status(statusCode.notFound).json(
+					response({
+						type: types.error,
+						message: 'Not found.'
+					})
+				)
+
+			return res.status(statusCode.success).json(
+				response({
+					type: types.success,
+					message: 'Access updated successfully.',
+					data
 				})
 			)
 		} catch (error) {
--- src/routes/user-management/access.js
+++ src/routes/user-management/access.js
@@ -10,10 +10,28 @@ accessRoute.post(
 	AccessController.createAccess
 )
 
+accessRoute.patch(
+	'/admin/access/update/:_id',
+	[Middleware.authentication, Middleware.isAdmin, Middleware.isValidObjectId],
+	AccessController.updateAccess
+)
+
 accessRoute.delete(
 	'/admin/access/delete/:_id',
 	[Middleware.authentication, Middleware.isAdmin, Middleware.isValidObjectId],
 	AccessController.removeAccess
+)
+
+accessRoute.get(
+	'/admin/access',
+	[Middleware.authentication, Middleware.isAdmin],
+	AccessController.getAllAccess
+)
+
+accessRoute.get(
+	'/admin/access/:_id',
+	[Middleware.authentication, Middleware.isAdmin, Middleware.isValidObjectId],
+	AccessController.getAccess
 )
 
 export default accessRoute
